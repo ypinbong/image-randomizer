5b6f364d36ba1cb501531f5ae1580fc083b50067
yong <yongpinbong@gmail.com>
2024-07-24 21:12:12 +0800

    Deploy

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/image-randomizer",
-  output: "export", // enables static exports
-  reactStrictMode: true,
-};
-
-export default nextConfig;
