5ec1e8e9f974c1822883981c868ab7e1a403001f
yong <yongpinbong@gmail.com>
2024-07-24 21:12:12 +0800

    Deploy

---
+++ out/_next/static/NBLIkbfno7NZVUjsv8mp2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/980-c1f2e15115120a56.js","static/chunks/pages/index-a9cc596d6416fac6.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
