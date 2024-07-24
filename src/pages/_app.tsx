67af886d040cd7d86e15e41c836f723277dfbe9f
yong <yongpinbong@gmail.com>
2024-07-24 19:51:32 +0800

    finished MVP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
+import styles from "@/styles/Home.module.css";
 import type { AppProps } from "next/app";
+import { SnackbarProvider } from 'notistack'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <SnackbarProvider
+        autoHideDuration={3000}
+        className={styles.code}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        maxSnack={3}
+      >
+        <Component {...pageProps} />
+      </SnackbarProvider>
+  );
 }
