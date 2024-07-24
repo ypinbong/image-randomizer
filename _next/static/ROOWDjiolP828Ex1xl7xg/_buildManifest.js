6105c6007e364863fefcc936f35b57b6ee619874
yong <yongpinbong@gmail.com>
2024-07-24 22:41:16 +0800

    Deploy

--- _next/static/ROOWDjiolP828Ex1xl7xg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/980-c1f2e15115120a56.js","static/chunks/pages/index-e52a90f7c6451b4b.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
